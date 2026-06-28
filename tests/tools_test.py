e8dc92205de7b69dd9559de9e548831d2fbaab89
Bryan <bryan54045@gmail.com>
2026-06-28 12:52:18 +0800

    test: update test

---
+++ tests/tools_test.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tools import download_arxiv_pdf, list_all_papers
+
+
+def test_download_arxiv_pdf():
+    pass
+
+def test_list_all_papers():
+    assert list_all_papers.invoke({}) == os.listdir('./tmp/')
